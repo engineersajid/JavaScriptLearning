3c9f707ec2054f2f3b48c166b0ee153a9f706505
sajid <developersajid.net@gmail.com>
2026-03-08 15:39:35 +0600

    second

--- oop/constructor/cons-fun.js
+++ oop/constructor/cons-fun.js
@@ -2,8 +2,18 @@ function BankAccount(customerName, balance = 0) {
   this.customerName = customerName;
   this.balance = balance;
   this.accountNumber = Date.now();
+
+  this.deposit = function (amount) {
+    this.balance += amount;
+  };
+  this.withdraw = function (amount) {
+    this.balance -= amount;
+  };
 }
 
 // new keyword need to use
 sajidAccount = new BankAccount("sajid");
+sajidAccount.deposit(3000);
+console.log(sajidAccount);
+sajidAccount.withdraw(2000);
 console.log(sajidAccount);
