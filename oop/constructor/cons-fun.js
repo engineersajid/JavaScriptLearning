3083a1305de86864aeeb81411ad576a82f868d3d
sajid <developersajid.net@gmail.com>
2026-03-08 15:41:37 +0600

    second

--- oop/constructor/cons-fun.js
+++ oop/constructor/cons-fun.js
@@ -3,6 +3,8 @@ function BankAccount(customerName, balance = 0) {
   this.balance = balance;
   this.accountNumber = Date.now();
 
+  // here deposit and withdraw method generated everytime when a object
+  //  create so it's take memory space so to resolve this issue we need to use prototype.
   this.deposit = function (amount) {
     this.balance += amount;
   };
